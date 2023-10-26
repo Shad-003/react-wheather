f7b7fa079a7fefc6ac15bb00fdf842bfe2955a95
Shad-003 <104814190+Shad-003@users.noreply.github.com>
2023-10-26 19:50:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import WheatherApp from './Components/WheatherApp/WheatherApp'
+
+const App = () => {
+  return (
+    <div className='App'>
+      <WheatherApp />
+    </div>
+  )
+}
+
+export default App
